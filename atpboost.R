1938585d9dedb84502d24b2a6f866446069bb7c1
DESKTOP-VOVR3D6\Jeremy <jefische86@gmail.com>
2022-03-18 18:04:13 -0400

    more updates

--- atpboost.R
+++ atpboost.R
@@ -318,7 +318,7 @@ write.csv(r_for_modeling, file='r_for_modeling.csv')
 # Modeling glm() #
 ##################
 filea <- 'C:/Users/blue_/Documents/Kaggle/atp_tennis/git_data/r_for_modeling.csv'
-r_for_modeling <- read.csv(file=filea, header = TRUE)
+r_for_modeling <- read.csv(file=filea)
 
 wimbledon_2016 <- 218
 wimbledon_2017 <- 372
@@ -336,9 +336,6 @@ train <- train[!is.na(train$P2_P_Win),]
 train <- train[!is.na(train$P1_P_Sets),]
 train <- train[!is.na(train$P2_P_Sets),]
 
-train$P1Wins <- factor(train$P1Wins)
-test$P1Wins <- factor(test$P1Wins)
-
 m1 <- glm(P1Wins ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
           data=train, family = "binomial")
 summary(m1)
@@ -353,50 +350,92 @@ sum(diag(table(p.win, test$P1Wins)))/sum(table(p.win, test$P1Wins))
 # Elo + ranking 81% accuracy
 # Elo + ranking + PWin + PSets 82% accuracy
 
-########################
-# Modeling gbm(), rf() #
-########################
+##################
+# Modeling gbm() #
+##################
 library(gbm) # gbm.perf()
 library(caret) # train(), trainControl(), getModelInfo(), resamples()
+source('build_tree.R') # converts gbm.object to data.tree for plotting
 
 m1.gbm <- gbm(P1Wins ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
               data=train, 
-              distribution = 'bernoulli', 
-              n.trees = 500, 
+              distribution = 'bernoulli',
+              n.trees = 3000, 
               interaction.depth = 2,
-              shrinkage = 0.05, 
+              shrinkage = 0.05,
               cv.folds = 5)
 
-summary(m1.gbm)
+m1.gbm
+summary(m1.gbm) # will not work if P1Wins is a factor, must be an integer for distribution='bernoulli'
 
+#Best num of iterations
+best.iter = gbm.perf(m1.gbm, method = 'OOB') #recommends 292
+best.iter = gbm.perf(m1.gbm, method = 'cv') #need cv.folds arg >1 in gbm() call for this. recommends 1101
+
+print(pretty.gbm.tree(m1.gbm, i.tree = 1))
+plot(build_tree(m1.gbm, i.tree = 1))
+
+# Predictions are on the canonical scale, which for binomial is the log-odds scale. 
+# You can use the type="response" when predicting to put it on the probability scale.
+p.boost <- predict(m1.gbm, newdata = test, type = 'response')
+p.boost <- round(p.boost)
+
+table(Predicted=p.boost, Actual=test$P1Wins)
+sum(diag(table(p.boost, test$P1Wins)))/sum(table(p.boost, test$P1Wins)) #81%
+
+#n.trees = 1101, accuracy = 79%
+
+###################################
+# Modeling gbm with caret train() #
+###################################
 
 getModelInfo()$gbm$parameters
 trainctrl <- trainControl(method = 'repeatedcv', number = 5, repeats = 3)
+trainctrl <- trainControl(method = 'cv', number = 5, returnResamp = 'all')
 
-m1.gbm.caret <- train(P1Wins ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
-                      data=train, method = "gbm", distribution = "bernoulli",
+#Note for classification caret needs a factor variable
+m1.gbm.caret <- train(as.factor(P1Wins) ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
+                      data=train, 
+                      method = "gbm", 
+                      distribution = "bernoulli",
+                      trControl = trainctrl,
                       verbose = FALSE)
 
 m1.gbm.caret
 summary(m1.gbm.caret)
-p.boost <- predict(m1.gbm.caret, newdata = test)
-p.boost
+p.boost.caret <- predict(m1.gbm.caret, newdata = test)
+p.boost.caret
 
-table(Predicted=p.boost, Actual=test$P1Wins)
-sum(diag(table(p.boost, test$P1Wins)))/sum(table(p.boost, test$P1Wins)) #80%
+table(Predicted=p.boost.caret, Actual=test$P1Wins)
+sum(diag(table(p.boost.caret, test$P1Wins)))/sum(table(p.boost.caret, test$P1Wins)) #80%
 
 
-myGrid <- expand.grid(n.trees = c(150, 300, 500),
+myGrid <- expand.grid(n.trees = c(1100),
                       interaction.depth = c(1, 2, 3, 4, 5, 6),
                       shrinkage = c(0.01, 0.10, 0.15),
                       n.minobsinnode = c(10))
 
-m2.gbm.caret <- train(P1Wins ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
-                      data=train, method = "gbm", distribution = "bernoulli",
-                      verbose = FALSE, tuneGrid = myGrid)
+m2.gbm.caret <- train(as.factor(P1Wins) ~ P1_Elo + P2_Elo + P1_Rank + P2_Rank + P1_P_Win + P2_P_Win + P1_P_Sets + P2_P_Sets, 
+                      data=train, 
+                      method = "gbm", 
+                      distribution = "bernoulli",
+                      trControl = trainctrl,
+                      verbose = FALSE, 
+                      tuneGrid = myGrid)
+
+m2.gbm.caret
+summary(m2.gbm.caret)
+p2.boost.caret <- predict(m2.gbm.caret, newdata = test)
+p2.boost.caret
+
+table(Predicted=p2.boost.caret, Actual=test$P1Wins)
+sum(diag(table(p2.boost.caret, test$P1Wins)))/sum(table(p2.boost.caret, test$P1Wins)) #81%
+confusionMatrix(p2.boost.caret, as.factor(test$P1Wins))
 
 
-#randomForest
+###########################
+# Modeling randomForest() #
+###########################
 m1.rf.caret <- train(P1Wins ~ P1_Elo + P2_Elo, data=train, method = "rf",
                      trControl = trainctrl)
 m1.rf.caret
